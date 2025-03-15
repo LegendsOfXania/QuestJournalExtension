b561badc7f3b44f65e63698216469345531b476f
Legends of Xania <legendsofxania@gmail.com>
2025-03-15 17:25:26 +0100

    Update and rename QuestJournalInitializer.kt to Singleton.kt

--- src/main/kotlin/fr/xania/questjournal/Singleton.kt
+++ src/main/kotlin/fr/xania/questjournal/Singleton.kt
@@ -4,17 +4,16 @@ import com.typewritermc.core.extension.Initializable
 import com.typewritermc.core.extension.annotations.Singleton
 import com.typewritermc.engine.paper.logger
 import com.typewritermc.engine.paper.plugin
-import fr.xania.questjournal.entries.action.OpenJournalEntry
+import fr.xania.questjournal.entries.action.OpenJournal
 
 @Singleton
 object Initializer : Initializable {
     override suspend fun initialize() {
-
-        plugin.server.pluginManager.registerEvents(OpenJournalEntry.Journal, plugin)
-        logger.warning("QuestJournalExtension is under development, some bugs may occur.")
+        plugin.server.pluginManager.registerEvents(OpenJournal.Journal, plugin)
+        logger.info("JournalExtension initialized.")
     }
 
     override suspend fun shutdown() {
-        // Do something when the extension is shutdown
+        logger.info("JournalExtension shutdown.")
     }
 }
