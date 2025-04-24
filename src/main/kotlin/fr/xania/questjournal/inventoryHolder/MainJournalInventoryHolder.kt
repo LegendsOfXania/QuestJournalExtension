742493d334086d5809ba39e23354ed967ae02671
LegendsOfXania <legendsofxania@gmail.com>
2025-04-24 20:24:37 +0200

    Add InventoryHolder(s)

--- src/main/kotlin/fr/xania/questjournal/inventoryHolder/MainJournalInventoryHolder.kt
+++ src/main/kotlin/fr/xania/questjournal/inventoryHolder/MainJournalInventoryHolder.kt
@@ -1,2 +1,14 @@
 package fr.xania.questjournal.inventoryHolder
 
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.InventoryHolder
+
+class MainJournalInventoryHolder : InventoryHolder {
+    private lateinit var inventory: Inventory
+    override fun getInventory(): Inventory {
+        fun setInventory(inventory: Inventory) {
+            this.inventory = inventory
+        }
+        return inventory
+    }
+}
